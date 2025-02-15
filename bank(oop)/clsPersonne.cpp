d3aa34f87dd510febeec13d89adc048c3e64cfdc
Zizitalimamoumar <fatimetoulimam009@gmail.com>
2025-02-15 16:36:47 +0000

    start

---
+++ bank(oop)/clsPersonne.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+#include<string>
+
+using namespace std;
+class clsPersonne {
+
+private:
+    string _fName;
+    string _lName;
+    string _email;
+    string _phone;
+public:
+
+string getFname(){
+    return _fName;
+}
+
+};
